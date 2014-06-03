2c268a2682fdbaf6f55217bfbc92751750db7be3
BPoole84 <bpoole084@gmail.com>
2014-06-03 13:53:03 -0400

    added php includes

---
+++ lab9/lab9.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<!--<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
+<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div class="container">
+  <?php include_once('includes/header.php'); ?>
+  
+  
+  <?php require_once('includes/sidebar1.php'); ?>
+  
+  <?php require_once('includes/article.php'); ?>
+  
+  
+  <?php require_once('includes/aside.php'); ?>
+  
+  <?php include('includes/footer.php'); ?>
+  
+  <!-- end .container --></div>
+</body>
+</html>
